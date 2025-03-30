5c557ab5a8155dd2e6a263a6c5cb2348347b6996
Alberto <14043560+wesbluemarine@users.noreply.github.com>
2025-03-30 20:42:32 +0200

    Check if calendar.sh is running

--- bin/calendar.sh
+++ bin/calendar.sh
@@ -3,7 +3,7 @@
 title=/home/makaba/Roms/calendar.rom
 executable="/home/makaba/.local/bin/uxnemu /home/makaba/Roms/calendar.rom"
 
-if pgrep uxn; then
+if pgrep -f 'calendar.sh'; then
   pkill -f calendar.rom; else
   if ! wlrctl toplevel activate title:"$title"; then
 	$executable
